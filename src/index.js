2b90fe4b6aeb5992db332bda2e17af58147be622
ankit8795 <aaasankityadav7458812901@gmail.com>
2024-04-01 12:23:54 +0530

    models

--- src/index.js
+++ src/index.js
@@ -5,4 +5,20 @@ dotenv.config({
     path : './env'
 })
 connectDB()
+.then(()=>{
+   app.listen(process.env.PORT || 8000, ()=>{
+
+    console.log(`server is runnig on port ${process.env.PORT} `)
+
+   }
+          
+    ) 
+
+
+})
+.catch((err) =>{
+
+    console.log(`error occured in mongodb connection ${err}`)
+
+})
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,18 @@
+class ApiResponse{
+
+    
+    constructor(
+        statusCode,
+        message = "success",
+        data
+      
+    ){
+
+       
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+   
+    }
+}
